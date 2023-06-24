6dcab7b1b82b7d9e874d86f3afe0f769df09be58
ihwenwen20 <ihwannh96@gmail.com>
2023-06-25 01:41:10 +0700

    edit sikit

--- src/components/Latihan1LifecycleHooks.jsx
+++ src/components/Latihan1LifecycleHooks.jsx
@@ -17,7 +17,10 @@
 // </button>
 // }
 
-// keluaran dari react saat kita menggunakan Hooks, default nya adalah array yang di dalamnya element Object. INGAT!
-// useState vs useRef +> useState selalu ngerirender ulang saat ke triger, sedangkan useRef tidak.
-// useRefc diperlukan untuk memanipulasi DOM. Lebih advance lagi ada useCallback dll.
-// useState. useEffet, useRef keriga hal tersebut disebut Lifecycle React Hooks, atau pendeknya disebut HOOKS.
+// keluaran atau output dari react saat kita menggunakan Hooks, default nya adalah array yang di dalamnya adalah element Object. INGAT!
+// useState vs useRef => useState selalu ngerirender ulang saat ke triger, sedangkan useRef tidak.
+// useRef diperlukan untuk memanipulasi DOM. Lebih advance lagi ada useCallback dll.
+// useState, useEffet, useRef keriga hal tersebut disebut Lifecycle React Hooks, atau pendeknya disebut HOOKS.
+// pada useEffect, jika kita tidak menambahkan [] pada parameter keduanya, maka akan merirender setiap kali useEffect dipanggil.
+// Sedangkan jika kita tambahkan [] pada parameter keduanya, maka akan dirender satu kali saja saat pertama kali dimuat.
+// Dan jika [isi nama state nya], maka useEffect akan merirender saat state yg di panggil pada useEffect nya terjadi perubahan. Atau saat onmounted.
